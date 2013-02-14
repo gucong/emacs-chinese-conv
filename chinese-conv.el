c47bb7c3949be56fbac1cd7f88d08bc55d6ad49b
gucong <gucong43216@gmail.com>
2013-02-13 22:01:39 -0600

    modified:   chinese-conv.el

--- chinese-conv.el
+++ chinese-conv.el
@@ -1,4 +1,5 @@
-;;; chinese-conv.el --- convert between simplified and traditional Chinese
+;;; chinese-conv.el --- Chinese Conversion,
+;;; eg. between tradition and simplified forms
 
 ;; Author: gucong <gucong43216@gmail.com>
 
@@ -18,35 +19,99 @@
 
 ;;; Commentary:
 
-;; This file work with cconv (http://code.google.com/p/cconv/) 
-;; All things are in UTF8 by default.
-;;   see `chinese-conv-alist' and `chinese-conv-default-from'
+;; This file works with opencc (http://code.google.com/p/opencc/)
+;; and/or cconv (http://code.google.com/p/cconv)
+
+;; Put this file into your load-path and the following into your
+;; ~/.emacs:
+;;   (require 'chinese-conv)
+
+;;; Changelog:
+
+;; 2013/02/13
+;;     * New feature: multiple backend.
+;;       Customizable through `chinese-conv-backend'.
+;;       Built-in support for opencc and cconv. More backends
+;;       can be added through `chinese-conv-backend-alist'
+;;     * New Command: `chinese-conv-replace'
+;;     * API change: `chinese-conv'
+;;
+;; 2012/01/28
+;;     * Initial commit
+;;
 
 ;;; Code:
 
-(defvar chinese-conv-target-alist
-  '(("simplified"  "UTF8-CN")
-    ("traditional" "UTF8-TW"))
-  "Alist of Chinese conversion target.")
-
-(defvar chinese-conv-program-path
-  "cconv"
-  "The path of cconv program")
-
-(defvar chinese-conv-default-from
-  "UTF8"
-  "Default encoding of Chinese conversion source")
-
 (defvar chinese-conv-temp-path
-  "/tmp/cconv.tmp"
+  "/tmp/chinese_conv.tmp"
   "temp file for Chinese conversion")
 
+;; opencc backend
+(defvar chinese-conv-opencc-program
+  "opencc"
+  "The opencc program path")
+
+(defvar chinese-conv-opencc-alist
+  '(("simplified"  "/usr/share/opencc/mix2zhs.ini")
+    ("traditional" "/usr/share/opencc/mix2zht.ini")
+    ("from Simplified Chinese to Traditional Chinese" "/usr/share/opencc/zhs2zht.ini")
+    ("from Simplified to phrases of Taiwan" "/usr/share/opencc/zhs2zhtw_p.ini")
+    ("from Simplified to variants of Taiwan" "/usr/share/opencc/zhs2zhtw_v.ini")
+    ("from Simplified to variants and phrases of Taiwan" "/usr/share/opencc/zhs2zhtw_vp.ini")
+    ("Standard Configuration for Conversion from Traditional Chinese to Simplified Chinese" "/usr/share/opencc/zht2zhs.ini")
+    ("from Traditional to phrases of Taiwan" "/usr/share/opencc/zht2zhtw_p.ini")
+    ("from Traditional to variants of Taiwan" "/usr/share/opencc/zht2zhtw_v.ini")
+    ("from Traditional to variants and phrases of Taiwan" "/usr/share/opencc/zht2zhtw_vp.ini")
+    ("from Taiwan to China phrases (Simplified)" "/usr/share/opencc/zhtw2zhcn_s.ini")
+    ("from Taiwan to China phrases (Traditional)" "/usr/share/opencc/zhtw2zhcn_t.ini")
+    ("from Taiwan to Simplified" "/usr/share/opencc/zhtw2zhs.ini")
+    ("from Taiwan to Traditional" "/usr/share/opencc/zhtw2zht.ini"))
+  "Alist of opencc conversions.")
+
+(defun chinese-conv-opencc-command (conv)
+  (let ((arg (cadr (assoc conv chinese-conv-opencc-alist))))
+    (if (null arg) (error "Undefined conversion")
+      (concat chinese-conv-opencc-program
+              " -i " chinese-conv-temp-path
+              " -c " arg))))
+
+;; cconv backend
+(defvar chinese-conv-cconv-program
+  "cconv"
+  "The cconv program path")
+
+(defvar chinese-conv-cconv-alist
+  '(("simplified"  "UTF8-CN")
+    ("traditional" "UTF8-TW")
+    ("Taiwan"      "UTF8-TW")
+    ("Hong Kong"   "UTF8-HK"))
+  "Alist of cconv conversions.")
+
+(defun chinese-conv-cconv-command (conv)
+  (let ((arg (cadr (assoc conv chinese-conv-cconv-alist))))
+    (if (null arg) (error "Undefined conversion")
+      (concat chinese-conv-cconv-program
+              " -f UTF8 " " -t " arg
+              " " chinese-conv-temp-path))))
+
+;; common
+(defun chinese-conv-get-alist (&optional backend)
+  (let ((l (cadr (assoc (or backend chinese-conv-backend)
+                        chinese-conv-backend-alist))))
+    (if (null l) (error "Undefined backend")
+      l)))
+
+(defun chinese-conv-get-command (&optional backend)
+  (let ((f (caddr (assoc (or backend chinese-conv-backend)
+                         chinese-conv-backend-alist))))
+    (if (null f) (error "Undefined backend")
+      f)))
+
 ;;;###autoload
-(defun chinese-conv (str to &optional from-arg interactive-p)
-  "Convert a Chinese string between simplified and traditional form.
+(defun chinese-conv (str conv &optional backend)
+  "Convert a Chinese string, eg. between simplified and traditional forms.
 STR is the string to convert.
-TO is the target of conversion, see `chinese-conv-target-alist'.
-FROM-ARG is the encoding of the source string, see `chinese-conv-default-from'"
+BACKEND is the backend to be used, see `chinese-conv-backend-alist'."
   (interactive
    (let* ((guess (or (and transient-mark-mode mark-active
                         (buffer-substring-no-properties
@@ -54,22 +119,52 @@ FROM-ARG is the encoding of the source string, see `chinese-conv-default-from'"
                    (current-word nil t)))
           (word (read-string (format "String to convert (default: %s): " guess)
                              nil nil guess))
-          (to (completing-read "Convert to: " chinese-conv-target-alist nil t)))
-     (list word to nil t)))
-  (let ((to-arg (cadr (assoc to chinese-conv-target-alist))))
-    (if (null to-arg)
-        (error "Undefined conversion target")
-      (with-temp-file chinese-conv-temp-path
-        (insert str "\n"))
-      (let ((result
-             (substring
-              (shell-command-to-string
-               (concat chinese-conv-program-path
-                       " -f " (or from-arg chinese-conv-default-from)  " -t " to-arg " "
-                       chinese-conv-temp-path))
-              0 -1)))
-        (if interactive-p
-            (message result)
-          result)))))
+          (conv (completing-read "Converion: "
+                                 (chinese-conv-get-alist) nil t)))
+     (list word conv nil)))
+  (with-temp-file chinese-conv-temp-path
+    (insert str "\n"))
+  (let ((result
+         (substring
+          (shell-command-to-string
+           (funcall (chinese-conv-get-command backend) conv))
+          0 -1)))
+    (if (called-interactively-p 'any)
+        (message result)
+      result)))
+
+;;;###autoload
+(defun chinese-conv-replace (start end conv &optional backend)
+  "Convert a Chinese string in place. See `chinese-con'."
+  (interactive
+   (let ((start (region-beginning))
+         (end (region-end))
+         (conv (completing-read "Conversion: "
+                                (chinese-conv-get-alist) nil t)))
+     (list start end conv)))
+  (let ((str (buffer-substring-no-properties start end)))
+    (kill-region start end)
+    (insert (chinese-conv str conv (or backend chinese-conv-backend)))))
+
+;; customization
+
+(defvar chinese-conv-backend-alist
+  `(("opencc" ,chinese-conv-opencc-alist ,#'chinese-conv-opencc-command)
+    ("cconv" ,chinese-conv-cconv-alist ,#'chinese-conv-cconv-command))
+  "An alist to provide essential information about backends in the format
+ ((BACKEND CONVERSION-ALIST COMMAND-GENERATOR) ... )
+
+CONVERSION-ALIST is in the format ((IDENTIFIER INFO ... ) ... ).
+The IDENTIFIER specifies a conversion direction and INFO provides
+information to be used by COMMAND-GENERATOR.  It is preferable to
+have \"simplified\" and \"traditional\" among the IDENTIFIERs.
+
+COMMAND-GENERATOR is a function that consumes a string to be
+matched with IDENTIFIER in CONVERSION-ALIST and produces a shell
+command in the form of a string.")
+
+(defvar chinese-conv-backend
+  "opencc"
+  "Backend of the conversion, see `chinese-conv-backend-alist'.")
 
 (provide 'chinese-conv)
